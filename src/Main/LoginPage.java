4e86a93b64f35af27ed25eb16f4a6afe5cecd22d
BM-TechID <mansyurj168@gmail.com>
2023-06-18 18:53:52 +0700

    Perubahan dikit

---
+++ src/Main/LoginPage.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author aditr
+ */
+public class LoginPage {
+    public static void main(String[] args){
+        
+        // here set Signup.java file as a Main File for Login
+        Signup log = new Signup();
+        log.show();
+        
+        
+        
+    }
+}
